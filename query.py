f6cde7eb62042bfb698006b8168b502f8518d145
eatrabyo <itthisak.prk@gmail.com>
2022-12-05 00:48:31 +0700

    -adding query function for stock alert

---
+++ query.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy import exc, text, bindparam
+import pandas as pd
+
+
+def query_stock_alert(engine):
+    try:
+        stmt = f"""SELECT p.product_name, p.stock from product p
+            where p.stock < 10"""
+        t = text(stmt)
+        df = pd.read_sql(t, con=engine)
+        return df
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        print(e.orig)
+        print(e.statement)
