1526f6e1b6f111caa090dd299b5451e4c41a95b5
eatrabyo <itthisak.prk@gmail.com>
2022-12-05 18:41:59 +0700

    - major changes for update_customer

--- update_data.py
+++ update_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from query import query_customer_by_id, query_customer_list
 from engine import main_db
+from update_func import update_email,update_phone
 
 def update_customer_tbl():
     while True:
@@ -20,13 +21,28 @@ def update_customer_tbl():
                 cus_update_menu_num = input('Please enter menu number: ')
 
                 if cus_update_menu_num == '1':
-                    new_phone = input('enter new phone number: ')
-                    print(new_phone)
+
+                    # check if phone number have length of 11 and start with 66
+                    while True:
+                        new_phone = input('enter new phone number: ')
+                        if len(new_phone) == 11 and new_phone.startswith('66',0,2) == True:
+                            break
+                        else:
+                            print('Please input valid phone number')
+                            continue
+
+                    # update phone no. to our db
+                    update_phone(engine=main_db,cus_id=customer_id,phone=new_phone)
+
+                    # print new data
                     new_cus_info = query_customer_by_id(main_db,customer_id)
                     print("\n",new_cus_info,"\n")
                 elif cus_update_menu_num == '2':
                     new_email = input('enter new email: ')
-                    print(new_email)
+                    # update email to our db
+                    update_email(engine=main_db,cus_id=customer_id,email=new_email)
+
+                    # print new data
                     new_cus_info = query_customer_by_id(main_db,customer_id)
                     print("\n",new_cus_info,"\n")
                 elif cus_update_menu_num == '3':
