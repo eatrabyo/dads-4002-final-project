9486fb9d8e082c6cae330a5cd7af288c54be0f33
eatrabyo <itthisak.prk@gmail.com>
2022-12-11 00:44:02 +0700

    -adding new query

--- query.py
+++ query.py
@@ -122,3 +122,30 @@ def query_product_name(engine,product_id):
         print(type(e))
         print(e.orig)
         print(e.statement)
+
+def query_new_customer(engine):
+    try:
+        stmt = """SELECT c.id as customer_id, c.customer_user,c.old_customer,c.phone_number,c.email from customer c
+                where c.id = (SELECT max(c2.id) from customer c2)"""
+        t = text(stmt)
+        df = pd.read_sql(t, con=engine)
+        df.set_index('customer_id',inplace=True)
+        return df
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        print(e.orig)
+        print(e.statement)
+
+def query_lastest_transaction(engine):
+    try:
+        stmt = """    SELECT m.transaction_id from main m
+            WHERE m.id = (SELECT max(m2.id) from main m2)"""
+        t = text(stmt)
+        df = pd.read_sql(t, con=engine)
+        lastest_trans = df['transaction_id'][0]
+        pre_fix, running_trans_id = lastest_trans.split('_')
+        return pre_fix, running_trans_id
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        print(e.orig)
+        print(e.statement)
