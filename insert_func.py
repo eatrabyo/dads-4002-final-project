544ad77b604f27ae9446c0e3f090aa053c09c27d
eatrabyo <itthisak.prk@gmail.com>
2022-12-19 08:55:58 +0700

    -minor changes

--- insert_func.py
+++ insert_func.py
@@ -4,7 +4,7 @@ from sqlalchemy import exc
 def insert_new_customer(df,engine):
     try:
         df.to_sql(name="customer",con=engine, if_exists='append', index=False)
-        print("Finished Adding new customer info.")
+        print("Finished adding new customer info.")
 
     except exc.SQLAlchemyError as e:
         print(type(e))
@@ -14,7 +14,7 @@ def insert_new_customer(df,engine):
 def insert_new_transaction(df,engine):
     try:
         df.to_sql(name="main",con=engine, if_exists='append', index=False)
-        print("Finished Adding new transaction.")
+        print("Finished adding new transaction.")
 
     except exc.SQLAlchemyError as e:
         print(type(e))
