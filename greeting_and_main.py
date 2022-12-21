b2c715350a4e662a8e2103422a30570fc4489ec2
LAPTOP-GRVQTK8D\Karuntarat <kkaruntarat@gmail.com>
2022-12-21 21:42:59 +0700

    Create function main+write main code more simple

--- greeting_and_main.py
+++ greeting_and_main.py
@@ -2,7 +2,7 @@ from datetime import date
 from engine import main_db
 from sqlalchemy import exc, text, bindparam
 import pandas as pd
-from greeting_and_main_func import stock_aleart_greeting
+from greeting_and_main_func import stock_aleart_greeting, main
 
 #Greeting
 print('\033[\n\n+1m'+'**** Welcome to Stock management DADs4002 ****'+'\033[0m')
@@ -26,25 +26,5 @@ stock_aleart_greeting(engine = main_db)
 
 #USER CHOOSING MANU >> add more text to tell user which manu they are in
 print('\n\nPlease select any of the following manu \n 1.Insert data \n 2.Update data \n 3.Delete data \n 4.See report \n 5.Exit')
-while True:
-  user = input('What do you like to do today? (type 1, 2, 3, 4, or 5): ')
-  if user in ['1','2','3','4','5']:
-    if user == '1':
-      print(f'\nYou are now in "Insert Data" page')
-      break
-    elif user == '2':
-      print(f'\nYou are now in "Update Data" page')
-      break
-    elif user == '3':
-      print(f'\nYou are now in "Delete Data" page')
-      break
-    elif user == '4':
-      print(f'\nYou are now in "See report" page')
-      break
-    elif user == '5':
-      print(f'\nGoodbye :)')
-      break
-    else:
-      print(f'\nPlease enter the correct menu (1-5)')
-  else:
-    continue
+user_respond = input('What do you like to do today? (type 1, 2, 3, 4, or 5): ')
+main(user=user_respond)
--- greeting_and_main_func.py
+++ greeting_and_main_func.py
@@ -22,3 +22,20 @@ def stock_aleart_greeting(engine):
         print(type(e))
         print(e.orig)
         print(e.statement)
+
+def main(user):
+    ##while True:
+        user = input('What do you like to do today? (type 1, 2, 3, 4, or 5): ')
+        if user in ['1','2','3','4','5']:
+            if user == '1':
+                print(f'\nYou are now in "Insert Data" page')
+            elif user == '2':
+                print(f'\nYou are now in "Update Data" page')
+            elif user == '3':
+                print(f'\nYou are now in "Delete Data" page')
+            elif user == '4':
+                print(f'\nYou are now in "See report" page')
+            elif user == '5':
+                print(f'\nGoodbye :)')
+        else:
+            print(f'\nPlease enter the correct menu (1-5)')
