32d25f5583c4653dc42634ff4f4a3780e058b651
LAPTOP-GRVQTK8D\Karuntarat <kkaruntarat@gmail.com>
2022-12-21 21:22:24 +0700

    delete float

--- greeting_and_main.py
+++ greeting_and_main.py
@@ -27,7 +27,7 @@ stock_aleart_greeting(engine = main_db)
 #USER CHOOSING MANU >> add more text to tell user which manu they are in
 print('\n\nPlease select any of the following manu \n 1.Insert data \n 2.Update data \n 3.Delete data \n 4.See report \n 5.Exit')
 while True:
-  user = float(input('What do you like to do today? (type 1, 2, 3, 4, or 5): '))
+  user = input('What do you like to do today? (type 1, 2, 3, 4, or 5): ')
   if user in ['1','2','3','4','5']:
     if user == '1':
       print(f'\nYou are now in "Insert Data" page')
