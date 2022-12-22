c831b4697b980d74b4c482d89f23dde4ffbc44bb
LAPTOP-GRVQTK8D\Karuntarat <kkaruntarat@gmail.com>
2022-12-22 22:38:40 +0700

    1st draft - backbone

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from engine import main_db
 from greeting_and_main_func import stock_aleart_greeting, login
-from 
+from insert_data import insert_customer_tbl
 
 def main():
 #Greeting
@@ -15,6 +15,7 @@ def main():
         stock_aleart_greeting(main_db)
         while True:
             while True:
+                print('\n\nPlease select any of the following manu \n 1.Insert data \n 2.Update data \n 3.Delete data \n 4.See report \n 5.Exit')
                 user = input('\nWhat do you like to do today? (type 1, 2, 3, 4, or 5): ')
                 if user in ['1','2','3','4','5']:
                     break
@@ -25,18 +26,62 @@ def main():
             if user == '1':
                 print(f'\nYou are now in "Insert Data" page')
                 print('Which data do you want to insert? \n 1. Customer \n2. Transaction \n 3.Exit ')
-                user_insert = input('')
-                ## if ==1:
-                    insert
-                ##break
+                while True:
+                    user_insert = input('Please select option above: ')
+                    if user_insert in ['1','2','3']:
+                        break
+                    else:
+                        print('Please enter the menu!!')
+                    continue
+    
+                if user_insert == '1':
+                    insert_customer_tbl()
+                elif user_insert == '2':
+                    print('Which data do you want to insert? \n 1. New Transaction \n2. New Product for Old Transaction \3.Exit')
+                    while True:
+                        user_insert_option_2 = input('Please select option above: ')
+                        if user_insert_option_2 in ['1','2','3']:
+                            break
+                        else:
+                            print('Please enter the menu!!')
+                            continue
+                    if user_insert_option_2 == '1':
+                        print('insert main table')
+                    if user_insert_option_2 == '2':
+                        print('insert main table for more products')
+                    if user_insert_option_2 == '3':
+                        break
+                elif user_insert == '3':
+                    pass
+            ##break
+
             elif user == '2':
                 print(f'\nYou are now in "Update Data" page')
+                print('Which data do you want to update? \n 1. Customer \n2. Product \n 3.Transaction \n4.Exit')
+                while True:
+                    user_update = input('Please select option above: ')                    
+                    if user_update in ['1','2','3','4']:
+                        break
+                    else:
+                        print('Please enter the menu!!')
+                        continue
+
+                if user_update == '1':
+                    print('update customer table')
+                elif user_update == '2':
+                    print('update product table')
+                elif user_update == '3':
+                    print('update Transaction table')
+                elif user_update == '4':
+                    pass            
                 ##break
             elif user == '3':
                 print(f'\nYou are now in "Delete Data" page')
+                print('delete transaction table')
                 ##break
             elif user == '4':
                 print(f'\nYou are now in "See report" page')
+                print('โค้ดใหม่')
                 ##break
             elif user == '5':
                 print(f'\nGoodbye :)')
@@ -58,10 +103,6 @@ def main():
         
         logout_time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
         ##logbook=
-
-
-
-
-              
+             
 main()
 
