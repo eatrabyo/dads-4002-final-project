d6fd6191bc44a589b6b6427ebf988f95698b6547
mmaiip <nattasorn.mai@gmail.com>
2022-12-23 22:21:21 +0700

    :D

--- mai_test.py
+++ mai_test.py
@@ -33,7 +33,7 @@ print(f'30 days rolling average of sale: {df}')
 print()
 
 # สินค้าขายดี 10 อันดับแรกของวันที่เลือก
-print(f'Top product sale:')
+print(f'Top 10 product sale:')
 df = query_top_sale_product(main_db)
 print(df)
 print()
@@ -49,114 +49,164 @@ print()
 #เลือก menu
 print('To explore further?')
 while True:
-    print('report menu: \n 1. sale report \n 2. percentage change in sale  \n 3. product top sale and top profit \n 4. showing data \n 5. exit')
-    report_menu = input('Please enter menu number: ')
-    if report_menu in ['1','2','3','4','5']:
-        break 
-    else:
-        print('Wrong menu number.')
-        print('Please select again from menu 1-5 as follow:')
-        continue
-
-if report_menu == '1':
     while True:
-        try:
-            startdate = input('Start date (yyyy-mm-dd): ')
-            validate(startdate)
-            break
-        except:
-            print("Invalid date and time format")
+        print('report menu: \n 1. sale report \n 2. percentage change in sale  \n 3. product top sale and top profit \n 4. Customer relationship \n 5. showing data \n 6. exit')
+        report_menu = input('Please enter menu number: ')
+        if report_menu in ['1','2','3','4','5','6']:
+            break 
+        else:
+            print('Wrong menu number.')
+            print('Please select again from menu 1-6 as follow:')
             continue
-
-    while True:
-        try:
-            stopdate = input('Stop date (yyyy-mm-dd): ')
-            validate(stopdate)
-            break
-        except:
-            print("Invalid date and time format")
-            continue
-        #print('end of date stop format prove')
-
-    # รายการยอดรวม สินค้า ตามวันที่เลือก
-    print(f'Sale summary between: {startdate} to {stopdate}')
-
-    df = query_tot_sale_day_sum(main_db,startdate,stopdate)
-    print(f'Total sale = {df}')
     print()
 
-    df = query_tot_sale_timerange(main_db,startdate,stopdate)
-    print(f'Sale of each day:')
-    print(df)
-    print()
+    if report_menu == '1':
+        while True:
+            try:
+                startdate = input('Start date (yyyy-mm-dd): ')
+                validate(startdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
 
-    df=query_avr_basket(main_db,startdate,stopdate=None)
+        while True:
+            try:
+                stopdate = input('Stop date (yyyy-mm-dd): ')
+                validate(stopdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+            #print('end of date stop format prove')
 
-elif report_menu == '2':
-    while True:
-        try:
-            startdate = input('Start date (yyyy-mm-dd): ')
-            validate(startdate)
-            break
-        except:
-            print("Invalid date and time format")
-            continue
+        # รายการยอดรวม สินค้า ตามวันที่เลือก
+        print(f'Sale summary between: {startdate} to {stopdate}')
 
-    while True:
-        try:
-            stopdate = input('Stop date (yyyy-mm-dd): ')
-            validate(stopdate)
-            break
-        except:
-            print("Invalid date and time format")
-            continue
-        #print('end of date stop format prove')
+        df = query_tot_sale_day_sum(main_db,startdate,stopdate)
+        print(f'Total sale = {df}')
+        print()
 
-    # สินค้าขายดี 10 อันดับแรกของวันที่เลือก
-    print(f'Top product sale:')
-    df = query_top_sale_product(main_db)
-    print(df)
-    print()
+        df = query_tot_sale_timerange(main_db,startdate,stopdate)
+        print(f'Sale of each day:')
+        print(df)
+        print()
 
-    #หมวดหมู่สินค้าขายดี 3 อันดับแรกของวันที่เลือก
-    print(f'Top 3 category sale:')
-    df = query_top_sale_cate(main_db,startdate,stopdate)
-    print(df)
-    print()
+        df=query_avr_basket(main_db,startdate,stopdate=None)
+
+    elif report_menu == '2':
+        while True:
+            try:
+                startdate = input('Start date (yyyy-mm-dd): ')
+                validate(startdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+
+        while True:
+            try:
+                stopdate = input('Stop date (yyyy-mm-dd): ')
+                validate(stopdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+        print()
+
+        # สินค้าขายดี 10 อันดับแรกของวันที่เลือก
+        print(f'Top product sale:')
+        df = query_top_sale_product(main_db)
+        print(df)
+        print()
+
+        #หมวดหมู่สินค้าขายดี 3 อันดับแรกของวันที่เลือก
+        print(f'Top 3 category sale:')
+        df = query_top_sale_cate(main_db,startdate,stopdate)
+        print(df)
+        print()
+        
+        #เป็นปัญหา!!!!
+        #สินค้าที่ทำกำไรสูงสุด 3 อันดับแรกของวันที่เลือก
+        print(f'Top 10 profitable product:')
+        df = query_top_profit(main_db,startdate,stopdate)
+        print(df)
+        print()
+
+
+    elif report_menu == '3':
+        while True:
+            try:
+                startdate = input('Start date (yyyy-mm-dd): ')
+                validate(startdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+
+        while True:
+            try:
+                stopdate = input('Stop date (yyyy-mm-dd): ')
+                validate(stopdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+            #print('end of date stop format prove')
+        print()
+
+        print(f'percentage change in sale: ')
+        print(f'day on day different')
+        df = query_percentage_change(main_db,startdate,stopdate)
+        print(df)
+
+
+    #CRM
+    elif report_menu == '4':
+        while True:
+            try:
+                startdate = input('Start date (yyyy-mm-dd): ')
+                validate(startdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+
+        while True:
+            try:
+                stopdate = input('Stop date (yyyy-mm-dd): ')
+                validate(stopdate)
+                break
+            except:
+                print("Invalid date and time format")
+                continue
+            #print('end of date stop format prove')
+        print()
+
+
+        print(f'top buyer: ')
+        df = query_crm_top5(main_db,startdate,stopdate)
+        print(df)
+        print()
+
+        print(f'number of new and old buyer: ')
+        df = query_crm_old(main_db,startdate,stopdate)
+        print(df)
+        print()
+        
+        df = query_crm_old_list(main_db,startdate,stopdate)
+        print(df)
+        print()
+
     
-    #เป็นปัญหา!!!!
-    #สินค้าที่ทำกำไรสูงสุด 3 อันดับแรกของวันที่เลือก
-    print(f'Top 10 profitable product:')
-    df = query_top_profit(main_db,startdate,stopdate)
-    print(df)
-    print()
-
-
-elif report_menu == '3':
     while True:
-        try:
-            startdate = input('Start date (yyyy-mm-dd): ')
-            validate(startdate)
+        loop_to_main = input("Would you like to see another report? (Y/N): ").lower()
+        if loop_to_main in ['y','n']:
             break
-        except:
-            print("Invalid date and time format")
+        else:
             continue
-
-    while True:
-        try:
-            stopdate = input('Stop date (yyyy-mm-dd): ')
-            validate(stopdate)
-            break
-        except:
-            print("Invalid date and time format")
-            continue
-        #print('end of date stop format prove')
-
-    print(f'percentage change in sale: ')
-    df = query_percentage_change(main_db,startdate,stopdate)
-    print(df)
-
-
-
-
-
+    if loop_to_main == 'y':
+        continue
+    elif loop_to_main == 'n':
+        print(f'back to main menu')
+        break
