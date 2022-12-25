3fbf5db824a7e8eaaeb66b784138560aa006098b
LAPTOP-GRVQTK8D\Karuntarat <kkaruntarat@gmail.com>
2022-12-25 15:20:38 +0700

    backup and log - Done

--- greeting_and_main_func.py
+++ greeting_and_main_func.py
@@ -4,7 +4,7 @@ from sqlalchemy import exc, text
 import pandas as pd
 from tabulate import tabulate
 
-from engine import main_db
+from engine import main_db, mysql_engine
 
 def stock_aleart_greeting(engine):
     try:
@@ -24,6 +24,8 @@ select
         print(f'\n----- STOCK ALERT ON {date.today()} -----')
         print(tabulate(df, headers='keys', tablefmt='psql'))
         safety_stock = 10
+
+
         while True:
             ack = input('\nPlease READ stock alert and acknowledge (type a to acknowledge): ').lower()
             if ack == 'a':
@@ -63,34 +65,30 @@ def login():
 
 def backup_all_data():
     with main_db.connect() as con:
-            cs = con.execute('SELECT * FROM customer')
-            with open('CustomerBackUp.txt', 'w', encoding='utf-8') as customer_backup:   
-                customer_backup.writelines(f'\n---- Backup Customer Data on {datetime.now()} ----\n\nid   customer_user    old_customer    phone_number     email  ')
-            for row in cs:
-                cs_detail = row[0],row[1],row[2],row[3],row[4]  
-                with open('CustomerBackUp.txt', 'a', encoding='utf-8') as customer_backup:   
-                    customer_backup.writelines(f'\n\n {cs_detail}')
+        pr = con.execute('SELECT * FROM product')
+        with open('ProductBackUp.txt', 'w', encoding='utf-8') as product_backup:   
+            product_backup.writelines(f'\n---- Backup Product Data on {datetime.now()} ----')
+            product_backup.writelines(f'\n\nid,product_category,product_name,product_cost,stock') 
+            for row in pr:
+                product_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}')
 
-            ms = con.execute('SELECT * FROM main')
-            with open('MainBackUp.txt', 'w', encoding='utf-8') as main_backup:   
-                main_backup.writelines(f'\n---- Backup Main Data on {datetime.now()} ----\n\ntransaction_id	    purchasing_time	    customer_id	    product_id	   price_per_unit	  unit	  destination_district	   destination_province	     postal_code')
-            for row in ms:
-                main_detail = row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]
-                with open('MainBackUp.txt', 'a', encoding='utf-8') as main_backup:   
-                    main_backup.writelines(f'\n\n {main_detail}')
-
-            ps = con.execute('SELECT * FROM product')
-            with open('ProductBackUp.txt', 'w', encoding='utf-8') as product_backup:
-                product_backup.writelines(f'---- Backup Product Data on {datetime.now()} ----\n\nid	    product_category	product_name	product_cost	stock')
-            for row in ps:
-                product_detail = row[0],row[1],row[2],row[3],row[4]
-                with open('ProductBackUp.txt', 'a', encoding='utf-8') as product_backup:   
-                    product_backup.writelines(f'\n\n{product_detail}')
-
-            pc = con.execute('SELECT * FROM product_category')
-            with open('ProductCategoryBackUp.txt', 'w', encoding='utf-8') as productcat_backup:
-                productcat_backup.writelines(f'---- Backup Product Category Data on {datetime.now()} ---- \n\nproduct_category   name')
+        ma = con.execute('SELECT * FROM main')
+        with open('MainBackUp.txt', 'w', encoding='utf-8') as main_backup:   
+            main_backup.writelines(f'\n---- Backup Main Data on {datetime.now()} ----')
+            main_backup.writelines(f'\n\ntransaction_id,purchasing_time,customer_id,product_id,price_per_uni,uni,destination_district,destination_province,postal_code') 
+            for row in ma:
+                main_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]},{row[7]},{row[8]}')
+        
+        cu = con.execute('SELECT * FROM customer')
+        with open('CustomerBackUp.txt', 'w', encoding='utf-8') as customer_backup:   
+            customer_backup.writelines(f'\n---- Backup Customer Data on {datetime.now()} ----')
+            customer_backup.writelines(f'\n\ncustomer_user,old_customer,phone_number,email') 
+            for row in cu:
+                customer_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}')
+        
+        pc = con.execute('SELECT * FROM customer')
+        with open('ProductCategoryBackUp.txt', 'w', encoding='utf-8') as product_cat_backup:   
+            product_cat_backup.writelines(f'\n---- Backup ProductCategory Data on {datetime.now()} ----')
+            product_cat_backup.writelines(f'\n\nproduct_category,name') 
             for row in pc:
-                productcat_detail = [row[0],row[1]]
-                with open('ProductCategoryBackUp.txt', 'a', encoding='utf-8') as productcat_backup:   
-                    productcat_backup.writelines(f'\n\n {productcat_detail}')
+                product_cat_backup.writelines(f'\n{row[0]},{row[1]}')
