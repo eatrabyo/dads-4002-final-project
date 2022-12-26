c1ea7ef7593c700f5d9e6ee843ff671945de7106
LAPTOP-GRVQTK8D\Karuntarat <kkaruntarat@gmail.com>
2022-12-26 21:00:43 +0700

    to include in file name

--- greeting_and_main_func.py
+++ greeting_and_main_func.py
@@ -19,14 +19,15 @@ select
     group by product.id
     order by stock asc;
 """
+        #stock slert
         t=text(stmt)
         df=pd.read_sql(t,con=main_db)
         table = df.set_index("id",inplace=True)
         print(f'\n----- STOCK ALERT ON {date.today()} -----')
         print(tabulate(df, headers='keys', tablefmt='psql'))
         
+        #to do list
         print('\n\nTo-do list')
-        
         subtract = pd.DataFrame(safety_stock - df['stock'])
         subtract.rename(columns = {'stock':'จำนวนที่ต้องซื้อเข้า'}, inplace = True)
         df2=subtract.merge(df['product_name'],left_index=True, right_index=True)
@@ -37,9 +38,9 @@ select
             if ack == 'a':
                 break
     except exc.SQLAlchemyError as e:
-        print(type(e))
-        print(e.orig)
-        print(e.statement)
+        print(type(e)) #type ของ error คืออะไร
+        print(e.orig) #error มาจากไหน
+        print(e.statement) #statement sql ที่เกิด error
 
 #check password
 def login():
@@ -63,36 +64,32 @@ def login():
                 continue               
         return auth, login_time, user_name    
     except exc.SQLAlchemyError as e:
-        print(type(e))
-        print(e.orig)
-        print(e.statement)
+        print(type(e)) #type ของ error คืออะไร
+        print(e.orig) #error มาจากไหน
+        print(e.statement) #statement sql ที่เกิด error
 
 def backup_all_data():
     with main_db.connect() as con:
         pr = con.execute('SELECT * FROM product')
-        with open('ProductBackUp.txt', 'w', encoding='utf-8') as product_backup:   
-            product_backup.writelines(f'\n---- Backup Product Data on {datetime.now()} ----')
+        with open(f'ProductBackUp {datetime.now}.txt', 'w', encoding='utf-8') as product_backup:   
             product_backup.writelines(f'\n\nid,product_category,product_name,product_cost,stock') 
             for row in pr:
                 product_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}')
 
         ma = con.execute('SELECT * FROM main')
-        with open('MainBackUp.txt', 'w', encoding='utf-8') as main_backup:   
-            main_backup.writelines(f'\n---- Backup Main Data on {datetime.now()} ----')
+        with open(f'MainBackUp {datetime.now}.txt', 'w', encoding='utf-8') as main_backup:   
             main_backup.writelines(f'\n\ntransaction_id,purchasing_time,customer_id,product_id,price_per_uni,uni,destination_district,destination_province,postal_code') 
             for row in ma:
                 main_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]},{row[7]},{row[8]}')
         
         cu = con.execute('SELECT * FROM customer')
-        with open('CustomerBackUp.txt', 'w', encoding='utf-8') as customer_backup:   
-            customer_backup.writelines(f'\n---- Backup Customer Data on {datetime.now()} ----')
+        with open(f'CustomerBackUp {datetime.now}.txt', 'w', encoding='utf-8') as customer_backup:   
             customer_backup.writelines(f'\n\ncustomer_user,old_customer,phone_number,email') 
             for row in cu:
                 customer_backup.writelines(f'\n{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}')
         
         pc = con.execute('SELECT * FROM customer')
-        with open('ProductCategoryBackUp.txt', 'w', encoding='utf-8') as product_cat_backup:   
-            product_cat_backup.writelines(f'\n---- Backup ProductCategory Data on {datetime.now()} ----')
+        with open(f'ProductCategoryBackUp {datetime.now}.txt', 'w', encoding='utf-8') as product_cat_backup:   
             product_cat_backup.writelines(f'\n\nproduct_category,name') 
             for row in pc:
                 product_cat_backup.writelines(f'\n{row[0]},{row[1]}')
